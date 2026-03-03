67c86f8957237f2a03e79057cdde978818033fc4
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2026-03-03 14:55:42 +0530

    initial commit

---
+++ sumEvenNumber.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class sumEvenNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = sc.nextInt();
+        int sum = 0;
+        for (int i = 2; i <= num; i = i + 2) {
+            sum = sum + i;
+        }
+        System.out.println("Sum:" + sum);
+    }
+}
