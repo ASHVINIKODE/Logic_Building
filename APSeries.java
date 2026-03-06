fdb896a14f58a52416e99509fb7352f29331a546
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2026-03-07 02:03:23 +0530

    initial commit

---
+++ APSeries.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class APSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int d = sc.nextInt();
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(a + i * d + " ");
+        }
+    }
+}
